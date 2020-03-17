7eed6472de5cf8505f4081ae945f511f0917bb8e
Dominic <viado.dominic.n@gmail.com>
2020-03-18 00:18:01 +0800

    create login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Navigation from './components/navigation'
 import Home from './routes/home'
 import Orders from './routes/orders'
+import {Login} from './routes/login'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
           <Switch>
               <Route path="/" exact={true} component={Home}/>
               <Route path="/orders" exact={true} component={Orders}/>
-              <Route path="/login" exact={true} component={Home}/>
+              <Route path="/login" exact={true} component={Login}/>
               <Route path="/signup" exact={true} component={Home}/>
               <Route path="/update-profile" exact={true} component={Home}/>
           </Switch>
